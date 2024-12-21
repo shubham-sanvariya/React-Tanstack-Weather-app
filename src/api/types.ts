18eaeb555fcc43270cd6c06a0912687bcdd0bcd4
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-12-21 21:41:38 +0530

    adding WeatherCondition type

--- src/api/types.ts
+++ src/api/types.ts
@@ -2,3 +2,10 @@ export interface Coordinates{
     lat: number;
     lon: number;
 }
+
+export interface WeatherCondition{
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+}
