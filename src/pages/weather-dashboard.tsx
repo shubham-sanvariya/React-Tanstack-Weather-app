e971934a0c00ab50fc65de863c39e5482938ee80
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-12-22 16:04:41 +0530

    use react-queries for fetching in weather-dashboard

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -3,12 +3,24 @@ import {AlertTriangle, MapPin, RefreshCw} from "lucide-react";
 import {useGeolocation} from "../hooks/use-geolocation.ts";
 import WeatherSkeleton from "../components/loading-skeleton.tsx";
 import {Alert, AlertDescription, AlertTitle} from "../components/ui/alert.tsx";
+import {useForecastQuery, useReverseGeocodeQuery, useWeatherQuery} from "../hooks/use-weather.ts";
 
 const WeatherDashboard = () => {
 
     const {coordinates, error: locationError, getLocation, isLoading: locationLoading} = useGeolocation();
 
-    console.log(coordinates);
+    const weatherQuery = useWeatherQuery(coordinates);
+    const forecastQuery = useForecastQuery(coordinates);
+    const locationQuery = useReverseGeocodeQuery(coordinates);
+
+    const handleRefresh = () => {
+        getLocation();
+        if (coordinates){
+            weatherQuery.refetch()
+            forecastQuery.refetch()
+            locationQuery.refetch()
+        }
+    }
 
     if (locationLoading) {
         return <WeatherSkeleton/>
@@ -44,6 +56,25 @@ const WeatherDashboard = () => {
         )
     }
 
+    if (weatherQuery.error || forecastQuery.error){
+        return (
+            <Alert variant="destructive">
+                <AlertTriangle className="h-4 w-4"/>
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription className={'flex flex-col gap-4'}>
+                    <p>Failed to fetch weather data. Please try again.</p>
+                    <Button onClick={handleRefresh} variant={'outline'} className={'w-fit'}>
+                        <RefreshCw className="mr-2 h-4 w-4"/>
+                        retry
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        )
+    }
+    if (!weatherQuery.data || !forecastQuery.data){
+        return <WeatherSkeleton/>;
+    }
+
     return (
         <div className={'space-y-4'}>
             {/*Favorite Cities*/}
@@ -52,10 +83,10 @@ const WeatherDashboard = () => {
                 <Button
                     variant={'outline'}
                     size={'icon'}
-                    // onClick={handleRefresh}
-                    // disabled={}
+                    onClick={handleRefresh}
+                    disabled={weatherQuery.isFetching || forecastQuery.isFetching}
                 >
-                    <RefreshCw className={'h-4 w-4'}/>
+                    <RefreshCw className={`h-4 w-4 ${weatherQuery.isFetching  ? 'animate-spin' : ""}`}/>
                 </Button>
 
             </div>
