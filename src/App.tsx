a1ad45c7e5ba98c82392451e2163951434f7a69f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-12-22 13:57:23 +0530

    adding error handling in weather-dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-
 import './App.css'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Layout from "./components/layout.tsx";
-import { ThemeProvider } from "./components/context/theme-provider.tsx";
+import {ThemeProvider} from "./components/context/theme-provider.tsx";
 import WeatherDashboard from "./pages/weather-dashboard.tsx";
 import CityPage from "./pages/city-page.tsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
@@ -10,23 +9,23 @@ import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 function App() {
 
-  const queryClient = new QueryClient();
+    const queryClient = new QueryClient();
 
-  return (
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <ThemeProvider>
-            <Layout>
-              <Routes>
-                <Route path='/' element={<WeatherDashboard/>}/>
-                <Route path='/city/:cityName' element={<CityPage/>}/>
-              </Routes>
-            </Layout>
-          </ThemeProvider>
-        </BrowserRouter>
-          <ReactQueryDevtools initialIsOpen={false}/>
-      </QueryClientProvider>
-  )
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <ThemeProvider>
+                    <Layout>
+                        <Routes>
+                            <Route path='/' element={<WeatherDashboard/>}/>
+                            <Route path='/city/:cityName' element={<CityPage/>}/>
+                        </Routes>
+                    </Layout>
+                </ThemeProvider>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
+    )
 }
 
 export default App
