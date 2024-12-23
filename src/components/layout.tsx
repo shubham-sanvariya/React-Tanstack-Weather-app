44c87cf52206d0d5593574217a0954fef704b6e0
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-12-23 18:28:40 +0530

    adding footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({children}: PropsWithChildren) => {
             </main>
             <footer className={'border-t backdrop-blur py-12 supports-[backdrop-filter]: bg-background/60'}>
                 <div className="container mx-auto px-4 text-center text-gray-400">
-                    <p>Made with love by shubh</p>
+                    <p>Made with 💗 by shubham sawariya</p>
                 </div>
             </footer>
         </div>
